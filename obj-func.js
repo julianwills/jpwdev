ab42e52a0d649cc21a900d565de76d14b01895be
julianwills <80351642+julianwills@users.noreply.github.com>
2024-11-08 00:13:53 +0000

    Initial

---
+++ obj-func.js
@@ -0,0 +1,15 @@
+
+
+// let obj = {value: 'obj item'};
+
+// console.log(obj);
+
+// const Circle2 = new Function ('radius', `
+//     this.radius = radius;
+//     this.draw = function() {
+//         console.log('draw');
+//     }
+//     `);
+
+//     const another = new Circle2(12);
+//     console.log(another);
